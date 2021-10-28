e86b619cfee63c006c151c840942e86e28bd5ba3
Буков Александр Викторович (DigitalSpirit) <abukov@digital-spirit.ru>
2021-10-28 18:47:34 +0300

    печать операций

--- src/main/java/com/edu/model/Wallet.java
+++ src/main/java/com/edu/model/Wallet.java
@@ -8,19 +8,24 @@ public class Wallet {
     private HashMap<String, Summary> money;
     private Converter bank;
 
+    private MoneyPrinter printer;
     public Wallet() {
         bank = new Bank();
         bank.addCurrency("RUB");
         bank.addCurrency("USD");
         money = new HashMap<>();
+        printer = new MoneyPrinter();
     }
 
     public Wallet(Converter converter) {
         money = new HashMap<>();
         bank = converter;
+
+        printer = new MoneyPrinter();
     }
 
     public void addMoney(String currency, double amount) {
+        printer.print("Зачисление", currency, amount);
         Summary mon = money.get(currency);
         if (mon == null) {
             mon = new Summary();
@@ -30,6 +35,7 @@ public class Wallet {
     }
 
     public double removeMoney(String currency, double amount) {
+        printer.print("Списание", currency, amount);
         Summary s = money.get(currency);
         if (s == null) {
             return 0;
